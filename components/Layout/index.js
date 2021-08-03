edb71379734f20eb9bfabdda751db82f58c1db88
duslerke <liudvikast@gmail.com>
2021-08-03 19:04:39 +0100

    Add Header component to Layout component.

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,5 +1,8 @@
+import Header from "./Header";
+
 const Layout = ({ children }) => (
   <>
+    <Header serviceName="Documentation for Hackney IT Projects" />
     <div className="govuk-width-container app-width-container">
       <main className="govuk-main-wrapper app-main-class" id="content" role="main">
         {children}

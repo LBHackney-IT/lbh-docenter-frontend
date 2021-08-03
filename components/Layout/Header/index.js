a87aa19aed7c8375e4a8814fdb72f267a867b40b
duslerke <liudvikast@gmail.com>
2021-08-03 19:03:49 +0100

    Add Hackney Logo to Header. Change Header bottom-bar colour.

--- components/Layout/Header/index.js
+++ components/Layout/Header/index.js
@@ -1,9 +1,14 @@
 import css from "./index.module.scss";
+import HackneyLogo from "./HackneyLogo";
 
 const Header = ({ serviceName }) => (
   <header className="govuk-header" role="banner" data-module="govuk-header">
     <div className={`govuk-header__container govuk-width-container ${css["lbh-header__container"]}`}>
-      <div className="govuk-header__logo">Logo Will be Here</div>
+      <div className="govuk-header__logo">
+        <span className={`govuk-header__logotype ${css["lbh-header__logotype"]}`}>
+          <HackneyLogo />
+        </span>
+      </div>
       <div className="govuk-header__content">
         <a href="/" className="govuk-header__link govuk-header__link--service-name">
           {serviceName}

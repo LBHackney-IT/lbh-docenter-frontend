60caa3184b5c2407c7ce2b6df5c436e3cf0f9705
duslerke <liudvikast@gmail.com>
2021-08-15 21:00:00 +0100

    Added rough unstyled docs page non-main content layout.

--- components/Layout/Docs/index.js
+++ components/Layout/Docs/index.js
@@ -1,9 +1,28 @@
 import Header from "../Header";
+import css from "./index.module.css";
 
 const DocsLayout = ({ children }) => (
   <>
-    <Header serviceName="Connected Documentation" />
-    <main>{children}</main>
+    <div className={css.docsPage}>
+      <Header serviceName="Connected Documentation" />
+      <div className={css.docsPageContent}>
+        <aside className={css.sidebarWrapper}>
+          <div className={css.sidebar}>
+            <a href="/" className="TODO-homeLink">
+              Maybe Link
+            </a>
+            <nav class={css.apiSelection}>
+              <ul className={css.apiNavLinksList}>
+                {[...Array(5).keys()].map((num) => (
+                  <li key={num}>API number {num}</li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+        </aside>
+        <main>{children}</main>
+      </div>
+    </div>
   </>
 );
 

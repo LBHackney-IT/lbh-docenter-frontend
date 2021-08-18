b8c60b52cd03d997c2b791a470b5954e645dca3f
duslerke <liudvikast@gmail.com>
2021-08-18 22:08:56 +0100

    Rough toc sidebar layout.
    It's too early to go into detailed styling yet.

--- components/Layout/Docs/index.js
+++ components/Layout/Docs/index.js
@@ -34,18 +34,21 @@ const DocsLayout = ({ children }) => (
             <div className={`${apiContent.col} ${apiContent.col3of4}`}>{children}</div>
             <div className={`${apiContent.col} ${apiContent.col1of4}`}>
               <div className={apiContent.toc}>
-                <ul
-                  style={{
-                    color: "blue",
-                    listStyleType: "none",
-                  }}
-                >
-                  {[...Array(10).keys()].map((num) => (
-                    <li key={num}>
-                      <a href="#">Section {num}</a>
-                    </li>
-                  ))}
-                </ul>
+                <div className={apiContent.tocBorder}>
+                  <p>Toc</p>
+                  <ul
+                    style={{
+                      color: "blue",
+                      listStyleType: "none",
+                    }}
+                  >
+                    {[...Array(10).keys()].map((num) => (
+                      <li key={num}>
+                        <a href="#">Section {num}</a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </div>
             </div>
           </div>

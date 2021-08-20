5eeedfa6c969f6044eb3075f012673c1a690ae05
duslerke <liudvikast@gmail.com>
2021-08-20 09:51:17 +0100

    Added dull, but dynamically generated endpoints.
    For the time being these are mere redirects, but ideally I would be able to generate fake specifications.

--- mock-server/mockServer.js
+++ mock-server/mockServer.js
@@ -12,6 +12,22 @@ const inMemoryDatabase = generateData(10, mockServerPort);
 const router = jsonServer.router(inMemoryDatabase);
 
 server.use(middlewares);
+
+const realUrl = inMemoryDatabase.apiRecords[0].baseUrl.staging;
+server.get(realUrl, function (_, res) {
+  // for now I only have time to a boring redirect to the same url
+  res.status(302).redirect(`${realUrl}swagger/index.html`);
+});
+
+inMemoryDatabase.apiRecords.slice(1).forEach((r) => {
+  console.log(r.baseUrl.staging);
+  const relativeUrl = r.baseUrl.staging.split(mockServerPort)[1];
+  server.get(relativeUrl, function (_, res) {
+    // for now I only have time to a boring redirect to the same url
+    res.status(302).redirect(`${realUrl}swagger/index.html`);
+  });
+});
+
 server.use(router);
 
 server.listen(mockServerPort, () => {

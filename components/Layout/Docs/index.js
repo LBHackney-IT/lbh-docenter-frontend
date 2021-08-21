6e399646a61aebf60230ab2f9478879cd286cc05
duslerke <liudvikast@gmail.com>
2021-08-21 17:12:36 +0100

    Extracted API links sidebar into a component.
    Moved the CSS module into styles folder. Not sure if it's good or bad, but I want to give it go & see if it's an organisational improvement.

--- components/Layout/Docs/index.js
+++ components/Layout/Docs/index.js
@@ -1,5 +1,5 @@
 import Header from "../Header";
-import HackneyLogo from "../Header/HackneyLogo";
+import MainSidebar from "../Sidebar/MainSidebar";
 import css from "./index.module.css";
 import apiContent from "./apiContent.module.css";
 
@@ -8,27 +8,7 @@ const DocsLayout = ({ children }) => (
     <div className={css.docsPage}>
       <Header serviceName="Connected Documentation" />
       <div className={css.docsPageContent}>
-        <aside className={css.sidebarWrapper}>
-          <div className={css.sidebar}>
-            <a href="/" className={css.sidebarLogo}>
-              <div className={css.pointlessContainer}>
-                <HackneyLogo withinHeader={false} />
-                <p className={css.sidebarServiceName}>Connected Documentation</p>
-              </div>
-            </a>
-            <nav class={css.apiSelection}>
-              <ul className={css.apiNavLinksList}>
-                {[...Array(25).keys()].map((num) => (
-                  <li key={num}>
-                    <a href="#" className={css.apiLink}>
-                      API number {num}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </div>
-        </aside>
+        <MainSidebar />
         <main className={apiContent.apiItemDocsContainer}>
           <div className={apiContent.row}>
             <div className={`${apiContent.col} ${apiContent.col3of4}`}>{children}</div>

7cd79e0514f3151db2dbe41006fc7a269541b58b
duslerke <liudvikast@gmail.com>
2021-08-23 22:34:32 +0100

    Added test md file that contains most markup I will ever need to support.
    Also added additional setup such as a list of TODOs & a yarn command to test the markdown parser directly quicker.

---
+++ utilities/markdownParser.js
@@ -0,0 +1,16 @@
+const realMarkdown = require("./realMD");
+const featuresMD = require("./spaceAllMarkup");
+
+function markdownToTree() {
+  // extract links
+  // extract emphasis
+  // extract strikthrough
+  // extract images
+  // extract inline quotes
+  // extract nested headers
+  // extract unordered list
+  // extract tables
+  // extract ordered list
+  // extract blockquotes
+  // extract task lists
+}
---
+++ utilities/spaceAllMarkup.js
@@ -0,0 +1,69 @@
+const markupList = `
+# Into the void, from the void
+
+For those who have seen the __Earth from space__, and for the hundreds and perhaps thousands more who will, the experience most
+certainly changes your perspective. The things that we *share* in our world are far more valuable than those which divide us.
+
+Planets | Galaxies
+------- | --------
+Neptune | Milky Way
+Uranus | Andromeda
+
+## The scale
+
+It suddenly struck me that that tiny pea, pretty and blue, was the Earth. I put up my thumb and shut one eye, and my thumb
+blotted out the planet Earth. I didn’t feel like a giant. I felt very, very small.
+
+### Time, the great compounder
+
+Science has not yet mastered prophecy. We predict too much for the _next year_ and yet far too little for the **next 10**.
+
+One can get a perspective by reading Isaac Asimov Foundation series:
+
+1. Prelude to Foundation (1988)
+2. Forward the Foundation (1993)
+3. Foundation (1951)
+   4. Foundation and Empire (1952)
+   5. Second Foundation (1953)
+   6. Foundation's Edge (1982)
+   7. Foundation and Earth (1986)
+
+We choose to \`go to the moon\` in this decade and do the other things, not because they are easy, but because they are hard,
+because that goal will serve to organize and measure the best of our energies and skills, because that challenge is one that
+we are willing to accept, one we are ~~unwilling to postpone~~, and one which we intend to win.
+
+* Mercury
+* [Venus](https://en.wikipedia.org/wiki/Venus)
+* Mars
+  * Phobos
+  * Deimos
+
+### Monumental Undertaking
+
+There can be no thought of finishing for ‘aiming for the stars.’ Both figuratively and literally, it is a task to occupy the
+generations. And no matter how much progress one makes, there is always the thrill of just beginning.
+
+A wise blockquote was once said:
+
+> As I stand out here in the wonders of the unknown at Hadley, I sort of realize there’s a fundamental truth to our nature, Man
+> must explore . . . and this is exploration at its greatest.
+
+![Uranus Dark Spot](https://upload.wikimedia.org/wikipedia/commons/5/52/Uranus_Dark_spot.jpg)
+
+## A Globe
+
+Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the
+qualities of a globe, _a round earth in which all the directions **eventually** meet_, in which there is no center because every
+point, or none, is center — an equal earth which all men occupy as equals. The airman’s earth, if free men make it, will be
+truly round: a globe in practice, not in theory.
+
+***To be the first to enter the cosmos***, to engage, single-handed, in an unprecedented duel with nature—could one dream of
+anything more?
+
+There can be no thought of finishing for ‘aiming for the stars.’ Both figuratively and literally, it is a task to occupy
+the generations. And no matter how much progress one makes, there is always the thrill of just beginning.
+
+We are all connected; To each other, biologically. To the earth, chemically. To the rest of the universe atomically.
+`;
+
+export default markupList;

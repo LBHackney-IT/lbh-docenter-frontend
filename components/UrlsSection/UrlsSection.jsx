2eeda4bd24557a63ef03f63e8aec869ac2d26475
duslerke <liudvikast@gmail.com>
2021-09-12 17:07:13 +0100

    Added copy to clipboard button & styled it.

--- components/UrlsSection/UrlsSection.jsx
+++ components/UrlsSection/UrlsSection.jsx
@@ -1,4 +1,5 @@
 import css from "../../styles/UrlsSection.module.css";
+const { copyToClipboard } = require("../../utilities/globalStateUtils");
 
 const UrlsSection = ({ baseUrl, environment, githubUrl }) => {
   const environmentKey = environment.toLowerCase();
@@ -8,10 +9,20 @@ const UrlsSection = ({ baseUrl, environment, githubUrl }) => {
         <tr>
           <th>Github url:</th>
           <td>{githubUrl ?? "Not provided"}</td>
+          <td className={css["clipboard-button-cell"]}>
+            <button className={css["clipboard-button"]} onClick={copyToClipboard}>
+              Copy
+            </button>
+          </td>
         </tr>
         <tr>
           <th>{environment}:</th>
           <td>{baseUrl[environmentKey] ?? "Not provided"}</td>
+          <td className={css["clipboard-button-cell"]}>
+            <button className={css["clipboard-button"]} onClick={copyToClipboard}>
+              Copy
+            </button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -20,3 +31,4 @@ const UrlsSection = ({ baseUrl, environment, githubUrl }) => {
 
 export default UrlsSection;
 
+//className={css["url-cell"]}
---
+++ utilities/globalStateUtils.js
@@ -0,0 +1,20 @@
+function copyToClipboard(event) {
+  /* Get the text field */
+  console.log(event);
+
+  //   var copyText = document.getElementById("myInput");
+
+  //   /* Select the text field */
+  //   copyText.select();
+  //   copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  //   /* Copy the text inside the text field */
+  //   navigator.clipboard.writeText(copyText.value);
+
+  //   /* Alert the copied text */
+  //   alert("Copied the text: " + copyText.value); el.querySelector("#child")
+}
+
+module.exports = {
+  copyToClipboard,
+};

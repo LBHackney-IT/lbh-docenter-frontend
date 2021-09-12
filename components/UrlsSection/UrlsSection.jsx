395ed9f2d1b3c7d9759bbd29c7dfd6c2a9bc8975
duslerke <liudvikast@gmail.com>
2021-09-12 18:35:33 +0100

    Added UrlsSection header.
    It's needed both because it would be useful to have it for the purposes of TOC, but also because compared to no header varian, the website looks off.

--- components/UrlsSection/UrlsSection.jsx
+++ components/UrlsSection/UrlsSection.jsx
@@ -4,28 +4,31 @@ const { copyUrlsSectionUrl } = require("../../utilities/globalStateUtils");
 const UrlsSection = ({ baseUrl, environment, githubUrl }) => {
   const environmentKey = environment.toLowerCase();
   return (
-    <table className={css["project-urls-table"]}>
-      <tbody>
-        <tr>
-          <th>Github url:</th>
-          <td className={`${css["url-cell"]} url-cell`}>{githubUrl ?? "Not provided"}</td>
-          <td className={css["clipboard-button-cell"]}>
-            <button className={css["clipboard-button"]} onClick={copyUrlsSectionUrl}>
-              Copy
-            </button>
-          </td>
-        </tr>
-        <tr>
-          <th>{environment}:</th>
-          <td className={`${css["url-cell"]} url-cell`}>{baseUrl[environmentKey] ?? "Not provided"}</td>
-          <td className={css["clipboard-button-cell"]}>
-            <button className={css["clipboard-button"]} onClick={copyUrlsSectionUrl}>
-              Copy
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div className={css["urls-section-container"]}>
+      <h2>Urls</h2>
+      <table className={css["project-urls-table"]}>
+        <tbody>
+          <tr>
+            <th>Github url:</th>
+            <td className={`${css["url-cell"]} url-cell`}>{githubUrl ?? "Not provided"}</td>
+            <td className={css["clipboard-button-cell"]}>
+              <button className={css["clipboard-button"]} onClick={copyUrlsSectionUrl}>
+                Copy
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <th>{environment}:</th>
+            <td className={`${css["url-cell"]} url-cell`}>{baseUrl[environmentKey] ?? "Not provided"}</td>
+            <td className={css["clipboard-button-cell"]}>
+              <button className={css["clipboard-button"]} onClick={copyUrlsSectionUrl}>
+                Copy
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 

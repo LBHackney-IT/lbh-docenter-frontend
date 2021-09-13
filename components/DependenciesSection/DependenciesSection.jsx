1e3c995a3b27a78f8cf3bbdd005b875773c5f79e
duslerke <liudvikast@gmail.com>
2021-09-13 12:12:26 +0100

    Created database dependency section & added its styling.

--- components/DependenciesSection/DependenciesSection.jsx
+++ components/DependenciesSection/DependenciesSection.jsx
@@ -2,7 +2,8 @@ import css from "../../styles/DependenciesSection.module.css";
 
 const DependenciesSection = ({ dependencies }) => {
   const dbDependency = dependencies?.databases[0];
-  //   const
+  const endpoints = dbDependency?.endpointsUsingIt;
+  console.log(dbDependency);
 
   return (
     <div className="dependencies-section">
@@ -10,6 +11,7 @@ const DependenciesSection = ({ dependencies }) => {
       <div className={css["database-dependencies-container"]}>
         <h3>Databases</h3>
         <div className={css["database-dependency"]}>
+          <h4>Database Info:</h4>
           <div className={css["names-row"]}>
             <dl className={css["key-val-pair"]}>
               <dt>Name:</dt>
@@ -29,6 +31,32 @@ const DependenciesSection = ({ dependencies }) => {
               <dt>Hosted at:</dt>
               <dd>{dbDependency.hostedAt}</dd>
             </dl>
+          </div>
+          <div className={css["endpoints-using-db-container"]}>
+            <h4>Endpoints using database:</h4>
+            <table className={css["endpoints-using-db"]}>
+              <thead>
+                {/* <tr>
+                  <th>Endpoints using database:</th>
+                </tr> */}
+                <tr>
+                  <th>Http method</th>
+                  <th>Path</th>
+                  <th>Name</th>
+                </tr>
+              </thead>
+              <tbody>
+                {endpoints.map((endpoint, i) => {
+                  return (
+                    <tr key={i}>
+                      <td>{endpoint.httpMethod}</td>
+                      <td>{endpoint.path}</td>
+                      <td>{endpoint.name}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>

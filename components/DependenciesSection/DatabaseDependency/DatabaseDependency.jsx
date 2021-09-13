17385d42423f738cf3e1b6420b30172cac485380
duslerke <liudvikast@gmail.com>
2021-09-13 12:30:46 +0100

    Extracted single database dependency into a separate component.

---
+++ components/DependenciesSection/DatabaseDependency/DatabaseDependency.jsx
@@ -0,0 +1,54 @@
+import css from "../../../styles/DatabaseDependency.module.css";
+
+const DatabaseDependency = ({ database }) => {
+  return (
+    <div className={css["database-dependency"]}>
+      <h4>Database Info:</h4>
+      <div className={css["names-row"]}>
+        <dl className={css["key-val-pair"]}>
+          <dt>Name:</dt>
+          <dd>{database.name}</dd>
+        </dl>
+        <dl className={css["key-val-pair"]}>
+          <dt>Alt-name:</dt>
+          <dd>{database.technicalName}</dd>
+        </dl>
+      </div>
+      <div className={css["database-info"]}>
+        <dl className={css["key-val-pair"]}>
+          <dt>Type:</dt>
+          <dd>{database.type}</dd>
+        </dl>
+        <dl className={css["key-val-pair"]}>
+          <dt>Hosted at:</dt>
+          <dd>{database.hostedAt}</dd>
+        </dl>
+      </div>
+      <div className={css["endpoints-using-db-container"]}>
+        <h4>Endpoints using database:</h4>
+        <table className={css["endpoints-using-db"]}>
+          <thead>
+            <tr>
+              <th>Http method</th>
+              <th>Path</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {database?.endpointsUsingIt.map((endpoint, i) => {
+              return (
+                <tr key={i}>
+                  <td>{endpoint.httpMethod}</td>
+                  <td>{endpoint.path}</td>
+                  <td>{endpoint.name}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default DatabaseDependency;
--- components/DependenciesSection/DependenciesSection.jsx
+++ components/DependenciesSection/DependenciesSection.jsx
@@ -1,64 +1,15 @@
 import css from "../../styles/DependenciesSection.module.css";
+import DatabaseDependency from "./DatabaseDependency/DatabaseDependency";
 
 const DependenciesSection = ({ dependencies }) => {
   const dbDependency = dependencies?.databases[0];
-  const endpoints = dbDependency?.endpointsUsingIt;
-  console.log(dbDependency);
 
   return (
     <div className="dependencies-section">
       <h2>Dependencies</h2>
       <div className={css["database-dependencies-container"]}>
         <h3>Databases</h3>
-        <div className={css["database-dependency"]}>
-          <h4>Database Info:</h4>
-          <div className={css["names-row"]}>
-            <dl className={css["key-val-pair"]}>
-              <dt>Name:</dt>
-              <dd>{dbDependency.name}</dd>
-            </dl>
-            <dl className={css["key-val-pair"]}>
-              <dt>Alt-name:</dt>
-              <dd>{dbDependency.technicalName}</dd>
-            </dl>
-          </div>
-          <div className={css["database-info"]}>
-            <dl className={css["key-val-pair"]}>
-              <dt>Type:</dt>
-              <dd>{dbDependency.type}</dd>
-            </dl>
-            <dl className={css["key-val-pair"]}>
-              <dt>Hosted at:</dt>
-              <dd>{dbDependency.hostedAt}</dd>
-            </dl>
-          </div>
-          <div className={css["endpoints-using-db-container"]}>
-            <h4>Endpoints using database:</h4>
-            <table className={css["endpoints-using-db"]}>
-              <thead>
-                {/* <tr>
-                  <th>Endpoints using database:</th>
-                </tr> */}
-                <tr>
-                  <th>Http method</th>
-                  <th>Path</th>
-                  <th>Name</th>
-                </tr>
-              </thead>
-              <tbody>
-                {endpoints.map((endpoint, i) => {
-                  return (
-                    <tr key={i}>
-                      <td>{endpoint.httpMethod}</td>
-                      <td>{endpoint.path}</td>
-                      <td>{endpoint.name}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        </div>
+        <DatabaseDependency database={dbDependency} />
       </div>
     </div>
   );

36c9405f856222d29eeffaf28e477534b8276166
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2021-09-25 12:26:17 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,4 @@ module.exports = {
   reactStrictMode: true,
   distDir: 'build/_next',
   target: 'server',
-  webpack: (config, { webpack, isServer }) => {
-        config.plugins.push(new webpack.IgnorePlugin(/.*\.test\.[jt]s$/));
-        // Fixes npm packages that depend on `fs` module
-        if (!isServer) {
-            config.node = {
-                fs: 'empty'
-            };
-        }
-        return config;
-    }
 }
